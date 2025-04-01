b7ca9e5601b32a64e9d3a9b145b1846d42f2ff1d
Tucker Ritti <45155694+tuckerritti@users.noreply.github.com>
2025-04-01 20:20:02 +0200

    Add Google Analytics component and integrate it into the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import MetaPixel from "./components/MetaPixel";
+import GoogleAnalytics from "./components/GoogleAnalytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GoogleAnalytics />
         <MetaPixel />
         {children}
       </body>
